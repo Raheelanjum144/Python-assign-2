c252aab81ae558fa38ef1cf1c499fd628bace254
Raheel Anjum <biztechsolutions144@gmail.com>
2024-11-30 18:39:45 +0500

    question no 1 solved

---
+++ question no1.py
@@ -0,0 +1,12 @@
+# Write a function to calculate the area of a circle given its radius.
+def circle_area(radius):
+  pi = 3.14159
+  area = pi * radius**2
+  return area
+
+# Example usage:
+radius = 5
+result = circle_area(radius)
+print("The approximate area of the circle is:", result)
+
+
